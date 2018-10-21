77738c8c1af16616109bfd93d257f4b039f44233
Patrick Austin <42253888+pwaustin@users.noreply.github.com>
2018-10-21 10:49:33 -0700

    Complete problem 171

--- 171.py
+++ 171.py
@@ -1,6 +1,20 @@
-class Solution(object):
-    def titleToNumber(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+def title_to_number(s):
+    """
+    Problem:
+    Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+    :type s: str
+    :rtype: int
+
+    Solution: This is effectively converting from coded base 26 to base 10. Reverse s, enumerate it, convert the chars
+    to corresponding ints, and build the decimal number as you would in any base conversion. Offset by -64 produces the
+    appropriate conversion for capital letters A-Z.
+    """
+
+    result = 0
+    s = s[::-1]
+
+    for exponent, char in enumerate(s):
+        result += (ord(char) - 64) * (26 ** exponent)
+
+    return result
